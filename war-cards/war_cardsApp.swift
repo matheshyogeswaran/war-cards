c8b858c0075cdcb3258c7ad2740c24599e8e9111
Mathesh <99591069+matheshyogeswaran@users.noreply.github.com>
2025-04-13 21:17:29 +0530

    Initial Commit

---
+++ war-cards/war_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_cardsApp.swift
+//  war-cards
+//
+//  Created by Mathesh Yogeswaran on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct war_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
